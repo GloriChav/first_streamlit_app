b606140621162f3e151096d952a89287d23da84c
GloriChav <155578934+GloriChav@users.noreply.github.com>
2024-01-04 17:05:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Manu')
 
-streamlit.text('Omega 3 & Blouebarry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
